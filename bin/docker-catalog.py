69ab52ebab0c8101f64cb84bef29355cef44d397
Cyril Jouve <jv.cyril@gmail.com>
2016-09-24 03:10:18 +0200

    fix deployment

--- bin/docker-catalog.py
+++ bin/docker-catalog.py
@@ -10,11 +10,17 @@ import sys
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--registry', required=True)
+    parser.add_argument('-p', '--path')
     opts = parser.parse_args()
 
     def get(path):
         url = 'https://{}/v2/{}'.format(opts.registry, path)
         return requests.get(url).json()
+
+    if opts.path:
+        import json
+        print(json.dumps(get(opts.path), indent=2))
+        return 0
 
     repos = get('_catalog')['repositories']
     for repo in repos:
@@ -22,7 +28,7 @@ def main():
         for tag in tags:
             print('{}:{}'.format(repo, tag))
 
-    return
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
